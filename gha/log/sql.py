75e67ff2b86e1f5155cefd99f05286d80fb137f4
Matthew Rocklin <mrocklin@gmail.com>
2012-07-02 16:00:34 -0500

    Sanitize sql text
    This could be a lot better

--- gha/log/sql.py
+++ gha/log/sql.py
@@ -1,5 +1,21 @@
+import re
+
 def stringify(s):
     return '"%s"'%s
+
+bad_chars = {'"', "'"}
+def sanitize(s):
+    for char in bad_chars:
+        s = s.replace(char, '\\'+char)
+    return s
+
+def chain(*fns):
+    def fn(x):
+        for subfn in fns:
+            x = subfn(x)
+        return x
+    return fn
+
 
 def insert_statement(d, table_name):
     keys = sorted(d.keys())
@@ -8,4 +24,4 @@ def insert_statement(d, table_name):
     return "INSERT INTO %s\n(%s)\nVALUES\n(%s);"%(
             table_name,
             ', '.join(keys),
-            ', '.join(map(stringify, values)))
+            ', '.join(map(chain(sanitize, stringify), values)))
